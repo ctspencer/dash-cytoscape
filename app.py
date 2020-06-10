cf81d150c91589c58ed6ca5b4f49516d70da551f
ctspencer <62153750+ctspencer@users.noreply.github.com>
2020-06-10 15:56:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+#Imports necessary libraries, files, and assigns handles
+
 import json
 
 import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
 import dash_cytoscape as cyto
 from demos import dash_reusable_components as drc
@@ -13,33 +16,50 @@ server = app.server
 
 
 # ###################### DATA PREPROCESSING ######################
-# Load data
-with open('demos/data/sample_network.txt', 'r') as f:
-    network_data = f.read().split('\n')
+#pandas is used to read in a tab-delimited data frame and store it as the variable network_data
+#the data frame contains text scraped from the 2019 IMHC Proceedings and can be easily modified
 
-# We select the first 750 edges and associated nodes for an easier visualization
-edges = network_data[:750]
+network_data = pd.read_csv('demos/data/test_network.txt', sep='\t')
+
+
+#Each colmumn in the pandas dataframe is assigned a variable (e.g. source, target, flavtext)
+#Note that source and target refers to the directionality of nodes and respective edges. It reads from source to target and creates a linkage
+#flavtext refers to the associated text of a specific target (description of best practice)
+source = network_data['Source']
+target = network_data['Target']
+flavtext = network_data['Text']
+
+#Columns are recombined with a tab delimiter
+source_target_flavtext = source + "\t" + target + "\t" + flavtext
+
+
+#Each row of the intial dataframe is converted to a list, you'll see why later
+edges = source_target_flavtext.to_list()
 nodes = set()
 
-following_node_di = {}  # user id -> list of users they are following
-following_edges_di = {}  # user id -> list of cy edges starting from user id
 
-followers_node_di = {}  # user id -> list of followers (cy_node format)
-followers_edges_di = {}  # user id -> list of cy edges ending at user id
 
+following_node_di = {}  # user id -> list of practices they are following
+following_edges_di = {}  # user id -> list of cy edges starting from practice id
+followers_node_di = {}  # user id -> list of practices (cy_node format)
+followers_edges_di = {}  # user id -> list of cy edges ending at practice id
 cy_edges = []
 cy_nodes = []
 
+
+
 for edge in edges:
-    if " " not in edge:
-        continue
+    #split the combined text frame by our tab delimiter into the respective variables
+    source, target, flavtext = edge.split("\t")
 
-    source, target = edge.split(" ")
-
+    #constructs dictionaries of edges, targets, and sources for visualization
     cy_edge = {'data': {'id': source+target, 'source': source, 'target': target}}
-    cy_target = {"data": {"id": target, "label": "User #" + str(target[-5:])}}
-    cy_source = {"data": {"id": source, "label": "User #" + str(source[-5:])}}
+    cy_target = {"data": {"id": target, "text": flavtext}}
+    cy_source = {"data": {"id": source, }}
 
+
+    #The rest of the lines for this chunk of code build dictionaries to keep track of
+    #the sources, targets, and edges needed for visualization
     if source not in nodes:
         nodes.add(source)
         cy_nodes.append(cy_source)
@@ -65,10 +85,14 @@ for edge in edges:
     followers_node_di[target].append(cy_source)
     followers_edges_di[target].append(cy_edge)
 
-genesis_node = cy_nodes[0]
+
+#This is the very first node displayed, the source of all other nodes
+genesis_node = {'data': {'id': 'Major Topics', 'label': 'Major-Topics', 'extra': " "}}
 genesis_node['classes'] = "genesis"
 default_elements = [genesis_node]
 
+
+#This is a stylesheet used for the base visualizations of nodes and edges
 default_stylesheet = [
     {
         "selector": 'node',
@@ -102,30 +126,34 @@ default_stylesheet = [
     {
         'selector': '.followingNode',
         'style': {
-            'background-color': '#FF4136'
+            'background-color': '#393e46',
+            "label": "data(id)"
+
         }
     },
     {
         'selector': '.followingEdge',
         "style": {
-            "mid-target-arrow-color": "red",
+            "mid-target-arrow-color": "blue",
             "mid-target-arrow-shape": "vee",
-            "line-color": "#FF4136",
+            "line-color": "#00adb5",
         }
     },
     {
         "selector": '.genesis',
         "style": {
-            'background-color': '#B10DC9',
+            'background-color': '#222831',
             "border-width": 2,
-            "border-color": "purple",
+            "border-color": "#393e46",
             "border-opacity": 1,
             "opacity": 1,
+            "width": 50,
+            "height": 50,
 
             "label": "data(label)",
-            "color": "#B10DC9",
+            "color": "#000000",
             "text-opacity": 1,
-            "font-size": 12,
+            "font-size": 15,
             'z-index': 9999
         }
     },
@@ -136,109 +164,84 @@ default_stylesheet = [
             "border-color": "black",
             "border-opacity": 1,
             "opacity": 1,
+            "width": 50,
+            "height": 50,
             "label": "data(label)",
             "color": "black",
-            "font-size": 12,
+            "font-size": 15,
             'z-index': 9999
         }
     }
 ]
 
 # ################################# APP LAYOUT ################################
+
+#Styles created for use by html.Div 
 styles = {
     'json-output': {
         'overflow-y': 'scroll',
-        'height': 'calc(50% - 25px)',
-        'border': 'thin lightgrey solid'
+        'overflow-x': 'auto',
+        'overflow-wrap': 'break-word',
+        'border': 'thin lightgrey solid',
+        'width' : '90vw'
     },
-    'tab': {'height': 'calc(98vh - 80px)'}
+    'tab': {'height': 'calc(98vh - 80px)', 'width': '90vw'}
 }
 
+#Call cytoscape to construct the layout and elements of the graph
 app.layout = html.Div([
     html.Div(className='eight columns', children=[
         cyto.Cytoscape(
             id='cytoscape',
             elements=default_elements,
+            layout={ "name": "cose"},
             stylesheet=default_stylesheet,
             style={
                 'height': '95vh',
-                'width': '100%'
+                'width': '100%',
+                'word-break': 'break-word',
+                'overflow-wrap': 'break-word'
             }
         )
     ]),
-
+#Create a tab to display data associated with a node (calls dictionary components)
     html.Div(className='four columns', children=[
         dcc.Tabs(id='tabs', children=[
-            dcc.Tab(label='Control Panel', children=[
-                drc.NamedDropdown(
-                    name='Layout',
-                    id='dropdown-layout',
-                    options=drc.DropdownOptionsList(
-                        'random',
-                        'grid',
-                        'circle',
-                        'concentric',
-                        'breadthfirst',
-                        'cose'
-                    ),
-                    value='grid',
-                    clearable=False
-                ),
-                drc.NamedRadioItems(
-                    name='Expand',
-                    id='radio-expand',
-                    options=drc.DropdownOptionsList(
-                        'followers',
-                        'following'
-                    ),
-                    value='followers'
-                )
-            ]),
-
-            dcc.Tab(label='JSON', children=[
+            dcc.Tab(label='Tap Data', children=[
                 html.Div(style=styles['tab'], children=[
-                    html.P('Node Object JSON:'),
+                    html.P('Node Data'),
                     html.Pre(
-                        id='tap-node-json-output',
+                        id='tap-node-data-json-output',
                         style=styles['json-output']
                     ),
-                    html.P('Edge Object JSON:'),
-                    html.Pre(
-                        id='tap-edge-json-output',
-                        style=styles['json-output']
-                    )
                 ])
             ])
-        ]),
+        ])
 
     ])
 ])
 
 
 # ############################## CALLBACKS ####################################
-@app.callback(Output('tap-node-json-output', 'children'),
-              [Input('cytoscape', 'tapNode')])
-def display_tap_node(data):
-    return json.dumps(data, indent=2)
+#Callbacks are unique to the cytoscape library and allow interactive updating
+#more information on callbacks can be found in the Dash Cytoscape reference guide    
 
 
-@app.callback(Output('tap-edge-json-output', 'children'),
-              [Input('cytoscape', 'tapEdge')])
-def display_tap_edge(data):
-    return json.dumps(data, indent=2)
+#dump data assocaited with a node into a textbox
+@app.callback(Output('tap-node-data-json-output', 'children'),
+              [Input('cytoscape', 'tapNodeData')])
+def displayTapNodeData(data):
 
 
-@app.callback(Output('cytoscape', 'layout'),
-              [Input('dropdown-layout', 'value')])
-def update_cytoscape_layout(layout):
-    return {'name': layout}
+        return json.dumps(data, indent=2)
 
 
+#The following chunk of code keeps track of which nodes have been expanded
+#based on the dictionaries constructed at the beginning
 @app.callback(Output('cytoscape', 'elements'),
               [Input('cytoscape', 'tapNodeData')],
-              [State('cytoscape', 'elements'),
-               State('radio-expand', 'value')])
-def generate_elements(nodeData, elements, expansion_mode):
+              [State('cytoscape', 'elements')])
+def generate_elements(nodeData, elements):
     if not nodeData:
         return default_elements
 
@@ -252,36 +255,19 @@ def generate_elements(nodeData, elements, expansion_mode):
             element['data']['expanded'] = True
             break
 
-    if expansion_mode == 'followers':
+    following_nodes = following_node_di.get(nodeData['id'])
+    following_edges = following_edges_di.get(nodeData['id'])
 
-        followers_nodes = followers_node_di.get(nodeData['id'])
-        followers_edges = followers_edges_di.get(nodeData['id'])
+    if following_nodes:
+        for node in following_nodes:
+            if node['data']['id'] != genesis_node['data']['id']:
+                node['classes'] = 'followingNode'
+                elements.append(node)
 
-        if followers_nodes:
-            for node in followers_nodes:
-                node['classes'] = 'followerNode'
-            elements.extend(followers_nodes)
-
-        if followers_edges:
-            for follower_edge in followers_edges:
-                follower_edge['classes'] = 'followerEdge'
-            elements.extend(followers_edges)
-
-    elif expansion_mode == 'following':
-
-        following_nodes = following_node_di.get(nodeData['id'])
-        following_edges = following_edges_di.get(nodeData['id'])
-
-        if following_nodes:
-            for node in following_nodes:
-                if node['data']['id'] != genesis_node['data']['id']:
-                    node['classes'] = 'followingNode'
-                    elements.append(node)
-
-        if following_edges:
-            for follower_edge in following_edges:
-                follower_edge['classes'] = 'followingEdge'
-            elements.extend(following_edges)
+    if following_edges:
+        for follower_edge in following_edges:
+            follower_edge['classes'] = 'followingEdge'
+        elements.extend(following_edges)
 
     return elements
 
